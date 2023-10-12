de64dcf590885a110e07328e1c5e8c52ffa51fe7
roma <147475322+ushkov777@users.noreply.github.com>
2023-10-12 11:35:17 +0300

    Add files via upload
    Мяч отскакивает от стенок, играют два игрока

--- ping-pong.py
+++ ping-pong.py
@@ -1,38 +1,40 @@
-from pygame import *
-'''Необходимые классы'''
-
-#класс-родитель для спрайтов
-class GameSprite(sprite.Sprite):
-   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
-       super().__init__()
-       self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
-       self.speed = player_speed
-       self.rect = self.image.get_rect()
-       self.rect.x = player_x
-       self.rect.y = player_y
-
-   def reset(self):
-       window.blit(self.image, (self.rect.x, self.rect.y))
-
-class Player(GameSprite):
-   def update_r(self):
-       keys = key.get_pressed()
-       if keys[K_UP] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_DOWN] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-   def update_l(self):
-       keys = key.get_pressed()
-       if keys[K_w] and self.rect.y > 5:
-           self.rect.y -= self.speed
-       if keys[K_s] and self.rect.y < win_height - 80:
-           self.rect.y += self.speed
-
-#игровая сцена:
-back = (200, 255, 255) #цвет фона (background)
-win_width = 600
-win_height = 500
-window = display.set_mode((win_width, win_height))
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from pygame import *
+... '''Необходимые классы'''
+... 
+... #класс-родитель для спрайтов
+... class GameSprite(sprite.Sprite):
+...    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+...        super().__init__()
+...        self.image = transform.scale(image.load(player_image), (wight, height)) #вместе 55,55 - параметры
+...        self.speed = player_speed
+...        self.rect = self.image.get_rect()
+...        self.rect.x = player_x
+...        self.rect.y = player_y
+... 
+...    def reset(self):
+...        window.blit(self.image, (self.rect.x, self.rect.y))
+... 
+... class Player(GameSprite):
+...    def update_r(self):
+...        keys = key.get_pressed()
+...        if keys[K_UP] and self.rect.y > 5:
+...            self.rect.y -= self.speed
+...        if keys[K_DOWN] and self.rect.y < win_height - 80:
+...            self.rect.y += self.speed
+...    def update_l(self):
+...        keys = key.get_pressed()
+...        if keys[K_w] and self.rect.y > 5:
+...            self.rect.y -= self.speed
+...        if keys[K_s] and self.rect.y < win_height - 80:
+...            self.rect.y += self.speed
+... 
+... #игровая сцена:
+... back = (200, 255, 255) #цвет фона (background)
+... win_width = 600
+... win_height = 500
+... window = display.set_mode((win_width, win_height))
 window.fill(back)
 
 #флаги, отвечающие за состояние игры
@@ -92,5 +94,4 @@ while game:
 
    display.update()
    clock.tick(FPS)
-
 
